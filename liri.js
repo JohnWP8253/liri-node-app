d6b2cbe0f7e7d147fd2f9e4363980ef88519bd42
John Pendergrass <john.pendergrass@me.com>
2020-04-09 18:27:00 -0500

    wrote working music function

--- liri.js
+++ liri.js
@@ -8,7 +8,7 @@ var axios = require("axios");
 // grab and read random.txt file
 var fs = require("fs");
 
-var moment = require('moment');
+var moment = require("moment");
 
 //grab the spotify package and initialize
 var Spotify = require("node-spotify-api");
@@ -19,6 +19,7 @@ var command = process.argv[2];
 var input = process.argv.slice(3).join(" ");
 console.log(command, input);
 
+// use of switch statement to switch between functions
 switch (command) {
   case "concert-this":
     concert();
@@ -35,15 +36,33 @@ switch (command) {
   default:
     console.log("Please submit a valid request");
 }
-
+// Function to call information from REST API for Bands In Town
 function concert() {
-    var concertURL = `https://rest.bandsintown.com/artists/${input}/events?app_id=codingbootcamp`;
+  var concertURL = `https://rest.bandsintown.com/artists/${input}/events?app_id=codingbootcamp`;
 
-    axios.get(concertURL).then(function(response){
-        for (var i = 0; i < response.data.length; i++){
-            
-            console.log(`\nVenue Name: ${response.data[i].venue.name}\nLocated in: ${response.data[i].venue.city}, ${response.data[i].venue.country} \nDate and Time: ${moment(response.data[i].datetime).format("MM/DD/YYYY LT")}\n\n########################################`);
+  axios.get(concertURL).then(function (response) {
+    for (var i = 0; i < response.data.length; i++) {
+      // Print information to terminal
+      console.log(
+        `\nVenue Name: ${response.data[i].venue.name}\nLocated in: ${
+          response.data[i].venue.city
+        }, ${response.data[i].venue.country} \nDate and Time: ${moment(
+          response.data[i].datetime
+        ).format("MM/DD/YYYY LT")}\n\n########################################`
+      );
+    }
+  });
+}
+
+function music() {
+    if (command === 'spotify-this-song' && process.argv[3] === undefined) {
+        input = 'Vogue';
+    };
+
+    spotify.search({type: 'track', query: input, limit: 1}, function(err, data){
+        if (err) {
+            return console.log(`An error occured: ${err}`);
         }
-    })
-    
+        console.log(`\n########################################\n\nArtist(s): ${data.tracks.items[0].artists[0].name}\nSong Name: ${data.tracks.items[0].name}\nPreview Link: ${data.tracks.items[0].preview_url}\nAlbum: ${data.tracks.items[0].album.name}\n\n########################################`)
+    });
 }
